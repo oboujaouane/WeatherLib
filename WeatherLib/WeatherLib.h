e1bca60c074515933325ee94dcc10f9843e9a953
Ousama boujaouane <oboujaouane@gmail.com>
2021-01-15 15:47:26 +0100

    Initial Commit

---
+++ WeatherLib/WeatherLib.h
@@ -0,0 +1,18 @@
+//
+//  WeatherLib.h
+//  WeatherLib
+//
+//  Created by ousama boujaouane on 15/01/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WeatherLib.
+FOUNDATION_EXPORT double WeatherLibVersionNumber;
+
+//! Project version string for WeatherLib.
+FOUNDATION_EXPORT const unsigned char WeatherLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeatherLib/PublicHeader.h>
+
+
