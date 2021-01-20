dd9f287078d5cb4d4f1fe094dcfbc1f561947f8f
Ousama boujaouane <oboujaouane@gmail.com>
2021-01-20 23:57:35 +0100

    Clean Cities view and add city view

--- WeatherLibExamples/Controller/CitiesViewController.swift
+++ WeatherLibExamples/Controller/CitiesViewController.swift
@@ -9,29 +9,18 @@ import UIKit
 import WeatherLib
 
 class CitiesViewController: UIViewController {
-    // MARK: - Life cycle
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-    }
     
-//         callWeatherService(for: 2988507) // Test with Paris id
-
-//    // MARK: - Private function
-//    private func callWeatherService(for cityId: Int) {
-//        WeatherService.shared().getWeather(for: cityId) { [weak self] success, weather in
-//            guard let self = self else { return }
-//
-//            if success, let weather = weather {
-//                print(weather.weather[0].weatherDescription.capitalizingFirstLetter())
-//                print("\(Int(weather.main.temp))°C")
-//                print("Humidité : \(weather.main.humidity)%")
-//            } else {
-//                self.presentAlert(title: "Petit problème",
-//                                  message: "Une erreur est survenue.\nVeuillez réessayer.")
-//            }
-//        }
-//    }
+    // MARK: - Properties
+    private var selectedCity: City?
+    private let segueIdentifier = "segueToCityDetail"
+    
+    // MARK: - Life cycle
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == segueIdentifier {
+            let cityDetailVC = segue.destination as! CityDetailViewController
+            cityDetailVC.city = selectedCity
+        }
+    }
     
 }
 
@@ -51,7 +40,8 @@ extension CitiesViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // TODO: ADD When detailCityViewController added
+        selectedCity = Cities.list[indexPath.row]
+        performSegue(withIdentifier: segueIdentifier, sender: self)
     }
 }
 
